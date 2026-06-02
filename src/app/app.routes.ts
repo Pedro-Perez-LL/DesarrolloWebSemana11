a277ab81ba6c16a266d55db86db7865ee0b45b42
Juan-Carlos-QH <u23211848@utp.edu.pe>
2026-06-01 21:56:49 -0500

    vista sobre-nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SobreNosotros} from './views/sobre-nosotros/sobre-nosotros';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sobre-nosotros', component: SobreNosotros }
+];

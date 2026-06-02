5faa26309e878ab11b534291666ea6229530c5cf
Pedro Luis Perez Llamas <u20211014@utp.edu.pe>
2026-06-01 22:07:12 -0500

    Se actualiza rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home'; // Tu archivo home.ts corto
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Esta es TU ruta oficial para la Tiendita de Don Pepe:
+  { path: '', component: HomeComponent }, 
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home'; // 
+import { RouterOutlet } from '@angular/router'; // 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent], // 
+  imports: [RouterOutlet], // 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
